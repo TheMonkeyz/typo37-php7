5e50d5be9d53c5ff5d14216e25e101010298900b
TheMonkeyz <rockmonkeyz@gmail.com>
2016-02-20 20:34:33 -0500

    Fixed wrong index.php link

--- web/typo3setup.sh
+++ web/typo3setup.sh
@@ -7,7 +7,7 @@ tar xzvf current -C /var/www/typo3_src --strip-components=1
 cd html
 ln -s ../typo3_src typo3_src
 ln -s typo3_src/typo3 typo3
-ln -s /typo3_src/index.php index.php
+ln -s typo3_src/index.php index.php
 touch FIRST_INSTALL
 chown -R www-data.www-data ../html
 
