dbc68123c1206ae710b9303a9c4b9955a5f70781
TheMonkeyz <rockmonkeyz@gmail.com>
2016-02-21 15:38:49 -0500

    Added some explaining

--- autosetup_centos.sh
+++ autosetup_centos.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+# Check if we're root
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
+# Bring the system up to date
 yum update -y
+
+# Install git
 yum install git -y
+
+# Install Docker
 curl -sSL https://get.docker.com/ | sh
 chkconfig docker on
 service docker start
+
+# Install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# Clone this repo and start up the environment
 git clone https://github.com/TheMonkeyz/typo37-php7.git
 cd typo37-php7
 /usr/local/bin/docker-compose up
-
-
